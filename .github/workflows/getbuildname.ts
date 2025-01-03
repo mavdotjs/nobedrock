6cab5ca167465558ee962898c934d998f0f49f68
Chad Freeman <ceo@marinadev.xyz>
2025-01-02 20:33:30 -0500

    new github action

---
+++ .github/workflows/getbuildname.ts
@@ -0,0 +1,13 @@
+import { setOutput } from "npm:@actions/core"
+import { TypedRegEx } from 'npm:typed-regex'
+
+const jarRegex = TypedRegEx('nobedrock-(?<version>(\\d\\.?){2,})-SNAPSHOT\\.jar')
+const buildlibspath = new URL('../../build/libs', import.meta.url).pathname
+
+for await (const dirEntry of Deno.readDir(buildlibspath)) {
+    if(!dirEntry.isFile) continue;
+    const match = jarRegex.match(dirEntry.name)
+    if(!match.matched) continue;
+    setOutput('version', match.groups?.version)
+    setOutput('filename', dirEntry.name)
+}
